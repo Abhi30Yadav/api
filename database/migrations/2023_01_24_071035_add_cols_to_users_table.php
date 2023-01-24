15ff344bccff2993e192eeda85af0bff632fb0d6
Your Name <you@example.com>
2023-01-24 15:14:54 +0530

    First Commit

---
+++ database/migrations/2023_01_24_071035_add_cols_to_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+           $table->string('contact')->nullable();
+           $table->text('address')->nullable();
+           $table->string('pincode',6);
+           $table->boolean('status')->comment('1:Actice and 2:Inactive')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+           $table->string('contact')->nullable();
+           $table->text('address')->nullable();
+           $table->string('pincode',6);
+           $table->boolean('status')->comment('1:Actice and 2:Inactive')->default(1);
+        });
+    }
+};
---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Route::get('users', function() {
+//     return "Hello World";
+// });
+// Route::post('users', function(){
+//     return response()->json("Api hit sucessfuly");
+// });
+
+// Route::delete('users/{id}',function($id){
+//     return response()->json($id,200);
+// });
+Route::get('/test', function (){
+    p('working');
+});
+
+Route::post('user/store',[App\Http\Controllers\Api\UserController::class, 'store']);
